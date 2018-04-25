ae331be0f636aab8855211a7c19b33362f7178e7
upshkin <upshkin@gmail.com>
2018-04-25 17:00:35 +0300

    Добавил Http404 для вывода сообщения, если question не найден

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 from .models import Question
@@ -9,11 +9,15 @@ def index(request):
     context = {
         'latest_question_list': latest_question_list,
     }
-    return render(request,'polls/index.html', context)
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
